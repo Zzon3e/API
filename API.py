985303c0d39f70732a9a5932e34c3680f2f5e9b5
Zzon3e <189995232+Zzon3e@users.noreply.github.com>
2025-12-16 15:16:38 +0100

    POKEMON UPG

--- API.py
+++ API.py
@@ -1,14 +1,17 @@
 import requests
+from pokemon import pokemon
 
 lenkeTilApi = "http://api.open-notify.org/astros.json"
 
 def main():
-    ting = input("Hva ønsker du å gjøre")
+    ting = input("Hva ønsker du å gjøre vær, catfact eller pokeapi")
 
-    if
-
-    vær = hentData("") #["main"]["temp"]
-    catfact = hentData("https://catfact.ninja/fact")["fact"]
+    if ting == "vær":
+        vær = hentData("https://api.openweathermap.or") ["26c9ad7e0adb187c0a7f698500326784"]
+    elif ting == "catfact":
+        catfact = hentData("https://catfact.ninja/fact")["fact"]
+    elif ting == "pokeapi":
+        pokemon()
 
 
 def hentData(link):
---
+++ pokemon.py
@@ -0,0 +1,8 @@
+from API import hentData
+def pokemon():
+    pokeapi = hentData("https://pokeapi.co/api/v2/pokemon/pikachu")
+    vekt = pokeapi["weight"]
+    høyde = pokeapi["height"]
+    print (vekt)
+    print (høyde)
+
